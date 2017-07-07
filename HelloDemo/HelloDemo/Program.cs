7da6543cf9111b62547a23fd77fb76696e9edfb6
Joseph6886 <dragon6886@msn.com>
2017-07-07 15:48:25 -0400

    1st Visual Basic user input information project

---
+++ HelloDemo/HelloDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HelloDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+
+            Console.WriteLine("It is raining today");
+            //Console.WriteLine("It is raining today");
+            //Console.ReadKey();
+            WriteLine("Lunch time soon");
+            WriteLine("Weekend soon!");
+            ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            //string contentation
+            Console.WriteLine("Hello " + name + " How are you today");
+            Console.ReadKey();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("You are a {0} year old guy", age);
+            //variable dropping into place
+            Console.WriteLine(" {0} You are a {1} year old guy am I right {2}", name,age, age);
+            Console.ReadLine();
+            Console.WriteLine("Are you going out tonight");
+            string goingOut = Console.ReadLine();
+            //string interpolation
+            Console.WriteLine($"My name is {name} I am {age} and {goingOut} of coarse");
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
