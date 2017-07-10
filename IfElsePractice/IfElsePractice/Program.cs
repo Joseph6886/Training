e96a13cc660c52b1ab266e49b1e28fe45f7258db
Joseph6886 <dragon6886@msn.com>
2017-07-10 16:49:47 -0400

    if else console

---
+++ IfElsePractice/IfElsePractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElsePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your favorite color?");
+            string favColor = Console.ReadLine().ToUpper();
+
+            if (favColor == "RED")
+            {
+                Console.WriteLine("red is good");
+               
+            }
+            else
+            {
+                Console.WriteLine("Whaterever");
+
+            }
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+
