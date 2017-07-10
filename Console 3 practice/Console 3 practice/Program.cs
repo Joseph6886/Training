514902f3c2f85d9276844a52f72165c3c61375ea
Joseph6886 <dragon6886@msn.com>
2017-07-10 15:59:41 -0400

    Meal and ice cream Console

---
+++ Console 3 practice/Console 3 practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_3_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite meal?");
+            string meal = Console.ReadLine();
+            Console.WriteLine("What is your favorite ice cream?");
+            string icecream = Console.ReadLine();
+            Console.WriteLine("How many scoops do you get?");
+            String scoops = Console.ReadLine();
+            Console.WriteLine($"Your favorite meal is {meal},your favorite ice cream is {icecream}, and you like this many scoops: {scoops}!");
+            Console.ReadLine();
+        }
+    }
+}
