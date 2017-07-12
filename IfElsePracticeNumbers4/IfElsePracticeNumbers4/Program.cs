4e6026d7aa5b8437bfd52a272f2b465ff16d85ca
Joseph6886 <dragon6886@msn.com>
2017-07-12 13:55:43 -0400

    welcome to Kohl's If/Else

--- IfElsePracticeNumbers4/IfElsePracticeNumbers4/Program.cs
+++ IfElsePracticeNumbers4/IfElsePracticeNumbers4/Program.cs
@@ -13,21 +13,21 @@ namespace IfElsePracticeNumbers4
             Console.WriteLine("What is your age?");
             double firstnum = double.Parse(Console.ReadLine());
 
-            if(firstnum > 0 && firstnum < 10)
+            if(firstnum > 0 && firstnum <= 10)
             { Console.WriteLine("You're just a kid");
             }
-            else if (firstnum > 11 && firstnum <20)
+            else if (firstnum > 11 && firstnum <=20)
             {
                 Console.WriteLine("You are in your middle years");
 
 
             }
-            else if (firstnum > 21 && firstnum < 35)
+            else if (firstnum >= 21 && firstnum <= 35)
             {
                 Console.WriteLine("You're getting there");
 
             }
-            else if (firstnum > 36 && firstnum <55)
+            else if (firstnum >= 36 && firstnum <55)
             {
                 Console.WriteLine("You have it figured out by now");
 
---
+++ IfElseVersion4/IfElseVersion4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseVersion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Kohl's, how much money do you have today?");
+            double firstnum = double.Parse(Console.ReadLine());
+
+            if (firstnum < 100 && firstnum >= 75)
+            {
+                Console.WriteLine("You can purchase anything you like!");
+            }
+            else if (firstnum <= 74 && firstnum >= 50)
+            {
+                Console.WriteLine("We have a nice pair of jeans for you");
+
+
+            }
+            else if (firstnum <= 49 && firstnum >= 25)
+            {
+                Console.WriteLine("You can't get much");
+
+            }
+            else if (firstnum <= 24 && firstnum > 0)
+            {
+                Console.WriteLine("GET OUT!");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Wow! your rich!!!");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
