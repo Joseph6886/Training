d1160d63d19449b921fda1ae2a4685dd85897934
Joseph6886 <dragon6886@msn.com>
2017-07-12 12:36:48 -0400

    ifelsenumbers4 practice

---
+++ IfElsePracticeNumbers4/IfElsePracticeNumbers4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElsePracticeNumbers4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            double firstnum = double.Parse(Console.ReadLine());
+
+            if(firstnum > 0 && firstnum < 10)
+            { Console.WriteLine("You're just a kid");
+            }
+            else if (firstnum > 11 && firstnum <20)
+            {
+                Console.WriteLine("You are in your middle years");
+
+
+            }
+            else if (firstnum > 21 && firstnum < 35)
+            {
+                Console.WriteLine("You're getting there");
+
+            }
+            else if (firstnum > 36 && firstnum <55)
+            {
+                Console.WriteLine("You have it figured out by now");
+
+
+            }
+            else
+            {
+                Console.WriteLine("You're in your golden years!!!");
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+}
