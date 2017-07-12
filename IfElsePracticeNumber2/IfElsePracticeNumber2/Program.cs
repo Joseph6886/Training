137a1aa3b668570767a0718f4640e5d710f7cd0d
Joseph6886 <dragon6886@msn.com>
2017-07-12 11:43:30 -0400

    If Else numbers practice 2 with no "string"

---
+++ IfElsePracticeNumber2/IfElsePracticeNumber2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElsePracticeNumber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a first number");
+            int number1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Give me a second number");
+            int number2 = int.Parse(Console.ReadLine());
+            
+
+            if (number1 > number2)
+            {
+                Console.WriteLine($"Your Number {number1} is greater than {number2}");
+            }
+            else if (number1 < number2)
+            {
+                Console.WriteLine($"Your first number {number1} is less than your second numner {number2}");
+            }
+            else
+            {
+                Console.WriteLine($"Your first number {number1} is equal to your second number of {number2}");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
