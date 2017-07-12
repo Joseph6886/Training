7ee40ee01fcd648e4b27b8aadc6c39d647b46f0a
Joseph6886 <dragon6886@msn.com>
2017-07-12 15:33:25 -0400

    Switch started version 1

---
+++ SwitchOrCase Version1/SwitchOrCase Version1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchOrCase_Version1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            switch(color)
+            {
+                case "red":
+                    Console.WriteLine("Red is an ok color");
+                    break;
+
+                default:
+                    Console.WriteLine("We don't understand your answer");
+                    break;
+                    
+
+            }
+        }
+    }
+}
