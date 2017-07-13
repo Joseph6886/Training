1088fa6c0b62d52210319b4e8606b091bb290eb2
Joseph6886 <dragon6886@msn.com>
2017-07-13 14:00:28 -0400

    example of switches

--- SwitchVersion3/SwitchVersion3/Program.cs
+++ SwitchVersion3/SwitchVersion3/Program.cs
@@ -10,10 +10,10 @@ namespace SwitchVersion3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your favorite color?");
+            Console.WriteLine("What is your favorite car?");
             string color = Console.ReadLine();
 
-            switch (color)
+            switch (car)
             {
                 case "red":
                     Console.WriteLine("Red is an ok color");
@@ -27,32 +27,7 @@ namespace SwitchVersion3
             }
             Console.ReadLine();
 
-            //int foodlike;
-            //Console.WriteLine("What's your favorite pasta dish \n 1. Lasagne \n 2. Spaghetti \n 3. Ravioli");
-            //// int foodchoice = Convert.ToInt32(ReadLine().ToUpper());
-            //string foodchoice = Console.ReadLine();
-            //// int foodchoice = int.Parse(ReadLine());
-            //int.TryParse(foodchoice, out foodlike);
-            //switch (foodlike)
-            //{
-            //    case 1:
-            //        Console.WriteLine("Lasagne is fantastic!!!");
-            //        break;
-            //    case 2:
-            //        Write("Spaghetti is delicious");
-            //        break;
-            //    case 3:
-            //        Console.Write("Ravioli was invented by Chef Boyardi");
-            //        break;
-            //    default:
-            //        Write("That is not a pasta choice!");
-            //        break;
 
-
-
-
-            //}
-            //Console.ReadLine();
 
 
         }
--- SwitchVersion4/SwitchVersion4/Program.cs
+++ SwitchVersion4/SwitchVersion4/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+
 
 namespace SwitchVersion4
 {
@@ -10,6 +12,31 @@ namespace SwitchVersion4
     {
         static void Main(string[] args)
         {
+
+            int foodlike = 0;
+                Console.WriteLine("What is your favorite pasta dish? \n 1. Lasagne \n 2. Spaghetti \n 3. Ravioli");
+            //foodlike = Convert.ToInt32(ReadLine().ToUpper());
+            string foodchoice = Console.ReadLine();
+            //int foodchoice = int.Parse(ReadLine());
+            int.TryParse(foodchoice, out foodlike);
+            //double food = Convert.ToDouble(ReadLine().ToUpper);
+
+            switch (foodlike)
+
+            { case 1:
+                Console.WriteLine("Lasagne is fantastic!");
+                break;
+            }
+
+            ReadLine();
+
+
+
+                
+
+
+
+
         }
     }
 }
