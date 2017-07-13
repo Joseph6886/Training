ec40d8ea0c1d9abc5eaf09f29f7fc1350f3bae82
Joseph6886 <dragon6886@msn.com>
2017-07-13 11:41:54 -0400

    Switch version 1

--- SwitchOrCase Version1/SwitchOrCase Version1/Program.cs
+++ SwitchOrCase Version1/SwitchOrCase Version1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Console; 
 namespace SwitchOrCase_Version1
 {
     class Program
@@ -25,6 +25,33 @@ namespace SwitchOrCase_Version1
                     
 
             }
+            Console.ReadLine();
+            
+            int foodlike;
+            Console.WriteLine("What's your favorite pasta dish \n 1. Lasagne \n 2. Spaghetti \n 3. Ravioli");
+            // int foodchoice = Convert.ToInt32(ReadLine().ToUpper());
+            string foodchoice = Console.ReadLine();
+            // int foodchoice = int.Parse(ReadLine());
+            int.TryParse(foodchoice, out foodlike);
+            switch (foodlike)
+            { case 1:
+                    Console.WriteLine("Lasagne is fantastic!!!");
+                    break;
+                case 2:
+                    Write("Spaghetti is delicious");
+                    break;
+                case 3:
+                    Console.Write("Ravioli was invented by Chef Boyardi");
+                    break;
+                default:
+                    Write("That is not a pasta choice!");
+                    break; 
+                    
+
+                    
+                    
+            }
+            Console.ReadLine();
         }
     }
 }
