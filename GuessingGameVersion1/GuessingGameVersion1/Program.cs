0ed7740f7a7be6ed4f04fd42fea97645b9e95bac
Joseph6886 <dragon6886@msn.com>
2017-07-18 11:38:51 -0400

    Guessing Game 1

---
+++ GuessingGameVersion1/GuessingGameVersion1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGameVersion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            // Random rnd = new Random();
+            Console.WriteLine("Hello world");
+
+            p.Working();
+            Talking();
+
+
+            Console.WriteLine(" I'm back in the main method!!!");
+            Console.ReadLine();
+
+
+
+        }
+        public void Working()
+        {
+            Console.WriteLine("Hey...I'm working over here!!!");
+           
+        }
+        public static void Talking()
+        {
+            Console.WriteLine("Talking a lot today");
+        }
+    }
+}
