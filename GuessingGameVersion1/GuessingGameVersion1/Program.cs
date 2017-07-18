14f589d6f752227d7a972188391d318c28106b88
Joseph6886 <dragon6886@msn.com>
2017-07-18 11:51:52 -0400

    added two new changes Playing and running

--- GuessingGameVersion1/GuessingGameVersion1/Program.cs
+++ GuessingGameVersion1/GuessingGameVersion1/Program.cs
@@ -17,6 +17,8 @@ namespace GuessingGameVersion1
 
             p.Working();
             Talking();
+            Playing();
+            Running();
 
 
             Console.WriteLine(" I'm back in the main method!!!");
@@ -28,11 +30,30 @@ namespace GuessingGameVersion1
         public void Working()
         {
             Console.WriteLine("Hey...I'm working over here!!!");
-           
+
         }
         public static void Talking()
         {
             Console.WriteLine("Talking a lot today");
         }
+        public static void Playing()
+        {
+            Console.WriteLine("Playing in Visual Studio");
+        }
+        public static void Running()
+        {
+        Console.WriteLine("Running in place");
+        }
+
+
     }
-}
+
+
+}       
+
+
+  
+
+    
+    
+
