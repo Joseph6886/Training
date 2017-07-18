56d3f808b0a4d716f852ee85a70dec9371d1674f
Joseph6886 <dragon6886@msn.com>
2017-07-18 10:12:15 -0400

    while loop numbers version 1

---
+++ WhileLoopNumbersVersion1/WhileLoopNumbersVersion1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopNumbersVersion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idNum;
+            int lowNum = 1000;
+            int highNum = 2000;
+
+            Console.WriteLine("Enter a valid ID Number");
+            string valNum = Console.ReadLine();
+            int.TryParse(valNum, out idNum);
+
+            while (idNum < lowNum || idNum > highNum)
+            {
+                Console.WriteLine($"The ID Number you entered is {valNum}\n" + $"Your ID number must be between {lowNum} and {highNum} \nTry again");
+
+                valNum = Console.ReadLine();
+                int.TryParse(valNum, out idNum);
+
+                
+            }
+            Console.WriteLine("The id number that you entered is valid");
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
