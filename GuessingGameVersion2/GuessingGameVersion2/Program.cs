93566e33ea997182f759e418805731a7e7656ccf
Joseph6886 <dragon6886@msn.com>
2017-07-18 16:17:56 -0400

    TestScores

---
+++ GuessingGameVersion2/GuessingGameVersion2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGameVersion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int rNum = rand.Next(1, 6);
+            int realNum;
+
+
+            Console.WriteLine("Enter a number between 1 and 5");
+            string numGuess = Console.ReadLine();
+
+            int.TryParse(numGuess, out realNum);
+            while (realNum < 0 || realNum > 6)
+            
+
+            while(realNum != rNum)
+            {
+                Console.WriteLine($"Your guess of {numGuess} was not the correct #...\nGuess again");
+                numGuess = Console.ReadLine();
+                int.TryParse(numGuess, out realNum);
+
+            }
+            Console.WriteLine($"Your guess of {rNum} was correct..Hoorey");
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ TestScores/TestScores/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idNum;
+            int lowNum = 0;
+            int highNum = 100;
+
+            Console.WriteLine("Enter a valid test score");
+            string valNum = Console.ReadLine();
+            int.TryParse(valNum, out idNum);
+
+            while (idNum < lowNum || idNum > highNum)
+            {
+                Console.WriteLine($"The score you entered is {valNum}\n" + $"Your score must be between {lowNum} and {highNum} \nTry again");
+                valNum = Console.ReadLine();
+                int.TryParse(valNum, out idNum);
+
+
+            }
+            Console.WriteLine("This score is valid");
+            Console.ReadLine();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+
+                if (fizz && buzz)
+                    Console.WriteLine("FizzBuzz");
+                else if (fizz)
+                    Console.WriteLine("Fizz");
+                else if (buzz)
+                    Console.WriteLine("Buzz");
+
+                else
+                    Console.WriteLine(i);
+                Console.ReadLine();
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
