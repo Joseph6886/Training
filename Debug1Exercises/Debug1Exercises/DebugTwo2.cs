a4377f3b8c3a72acbefaa5d1d17e2ff24f8ebc7d
Joseph6886 <dragon6886@msn.com>
2017-07-20 09:34:53 -0400

    Debugging practice

---
+++ Debug1Exercises/Debug1Exercises/DebugTwo2.cs
@@ -0,0 +1,22 @@
+// This program greets the user
+// and multiplies two entered values
+using System;
+using static System.Console;
+class DebugTwo2
+{
+   static void Main()
+   {
+      string name;
+      string first, second;
+      int.TryParse(first, out second) product;
+      WriteLine("Enter your name");
+      name = ReadLine();
+      WriteLine("Hello {0}! Enter an integer", name);
+      first = ReadLine();
+      WriteLine("Enter another integer");
+      second = ReadLine();
+      product = x * y;
+      WriteLine("Thank you {1}. The product of {2} and {3} is {4}",
+	 name, first, second, product);
+   }
+}
