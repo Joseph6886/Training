5566158582c1dce922be55ae008baf0d0e9c859c
Joseph6886 <dragon6886@msn.com>
2017-07-20 11:58:26 -0400

    Methhod practice 1 with reastaurants and fashion

---
+++ MethodPractice1/MethodPractice1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Restaurants r = new Restaurants();
+            Fashion f = new Fashion();
+
+            Console.WriteLine("This is the main method landing page");
+
+            r.Chipotle();
+            f.Pants();
+
+
+            Console.ReadLine();
+
+
+
+        }
+
+        public void Addition()
+        {
+
+
+
+        }
+        public void Multiplication()
+        {
+
+
+
+        }
+        public void Division()
+        {
+
+
+
+        }
+
+    }
+    class Fashion
+    {
+        //this is a method without static
+        public void Shirts()
+        {
+            Console.WriteLine("Button downs");
+            Console.WriteLine("V-neck");
+            Console.WriteLine("Henleys");
+        }
+        public void Pants()
+        {
+            Console.WriteLine("Bell Bottoms");
+
+        }
+        public void Shoes()
+        {
+
+
+        }
+
+    }
+
+    
+}
---
+++ MethodPractice1/MethodPractice1/Restaurants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice1
+{
+    class Restaurants
+    {
+        public static void FamousDaves()
+        {//these are instances that we create
+            Fashion fash = new Fashion();
+            Program p = new Program();
+            Restaurants r = new Restaurants();
+            //calling instances: Fashion, restaurants, etc.
+
+            fash.Pants();
+            fash.Shirts();
+            r.Chipotle();
+
+        }
+        public void Chipotle()
+        {
+            Console.WriteLine("We make burritos");
+
+        }
+        public void Benihana()
+        {
+            Console.WriteLine("We cook the food right in front of you");
+
+        }
+
+
+
+}
+}
