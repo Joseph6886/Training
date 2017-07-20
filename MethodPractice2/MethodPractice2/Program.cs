14ea09992dd7976cd34b887b53b00ee2ccaee3b8
Joseph6886 <dragon6886@msn.com>
2017-07-20 12:26:54 -0400

    method practice 2

--- MethodPractice2/MethodPractice2/Program.cs
+++ MethodPractice2/MethodPractice2/Program.cs
@@ -10,6 +10,34 @@ namespace MethodPractice2
     {
         static void Main(string[] args)
         {
+
+            Program p = new Program();
+            
+            int val1;
+            int val2;
+            Console.WriteLine("Give me the first number to multiply");
+            string num1 = Console.ReadLine();
+            int.TryParse(num1, out val1);
+
+            Console.WriteLine("Give me a second number to multiply");
+            string num2 = Console.ReadLine();
+            int.TryParse(num2, out val2);
+
+            p.Multiply(val1, val2);
+
+            Console.ReadLine();
+
+
         }
+
+        public void Multiply(int val1, int val2)
+        {
+            int product = val1 * val2;
+
+            Console.WriteLine($"The product of {val1} and {val2} is {product}");
+
+
+        }
+
     }
 }
