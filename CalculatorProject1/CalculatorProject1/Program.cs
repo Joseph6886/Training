0c899e4847ac6d03a80aea90398b5fb1f855738a
Joseph6886 <dragon6886@msn.com>
2017-07-20 22:17:17 -0400

    calc update

--- CalculatorProject1/CalculatorProject1/Program.cs
+++ CalculatorProject1/CalculatorProject1/Program.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace CalculatorProject1
 {
-    
- enum Calculator
+
+    enum Calculator
     {
         Addition = 1,
         Subtraction,
@@ -45,96 +45,105 @@ namespace CalculatorProject1
                     break;
                 default:
                     break;
-
+                    
             }
+        }
 
-            Console.WriteLine("Out of number switch");
+        public static void Add()
+        {
+
+            Console.WriteLine("Addition is fun!, Coding is hard");
             Console.ReadLine();
 
         }
-        //this is an arguement
-
-        public void Work()
+        public static void Sub()
         {
-            Program p = new Program();
-            int val1;
-            int val2;
-
-
-
+            Console.WriteLine("Subtraction is fun!, Coding is hard");
+            Console.ReadLine();
+        }
+        public static void Mul()
+        {
+            Console.WriteLine("Multiplication is fun!, Coding is hard");
+            Console.ReadLine();
+        }
+        public static void Div()
+        {
+            Console.WriteLine("Division is fun!, Coding is hard");
+            Console.ReadLine();
 
         }
-
-
-
-
-
-
-
-//        public static void Add(int val1, int val2)
-//        {
-
-//            Console.WriteLine("Give me the first number to execute");
-//            string num1 = Console.ReadLine();
-//            int.TryParse(num1, out val1);
-
-
-//        }
-//        public static void Sub()
-//        {
-//            Console.WriteLine("Give me a second number to execute");
-//            string num2 = Console.ReadLine();
-//            int.TryParse(num2, out val2);
-
-//        }
-//        public static void Mul()
-//        {
-//            Console.WriteLine("Give me a third number to execute");
-//            string num3 = Console.ReadLine();
-//            int.TryParse(num3, out val3);
-
-//        }
-//        public static void Div()
-//        {
-//            Console.WriteLine("Give me a second number to execute");
-//            string num2 = Console.ReadLine();
-//            int.TryParse(num2, out val2);
-
-//        }
-
-
-
-
-
-
-
-
-
-
-
-
-//        //These are peramiters
-//        public void Multiply(int val1, int val2)
-//        {
-//            int product = val1 * val2;
-
-//            Console.WriteLine($"The product of {val1} and {val2} is {product}");
-
-
-//        }
-//        public void Addition(int val1, int val2)
-//        {
-//            int total = val1 + val2;
-//            Console.WriteLine($"The product of {val1} added to {val2} is {total}");
-//        }
-//        public void Subtraction(int val1, int val2)
-//        {
-//            int answer = val1 - val2;
-//            Console.WriteLine($"The product of {val1} added to {val2} is {answer}");
-
- //       }
     }
 }
+        //this is an arguement
+
+        //public static void Add(int val1, int val2)
+        //{
+        //    Program p = new Program();
+
+        //    int val1;
+        //    int val2;
+        //    Console.WriteLine("Give me the first number to execute");
+        //    string num1 = Console.ReadLine();
+        //    int.TryParse(num1, out val1);
+
+
+
+            //    }
+            //    public static void Sub()
+            //    {
+            //        Console.WriteLine("Give me a second number to execute");
+            //        string num2 = Console.ReadLine();
+            //    int.TryParse(num2, out val2);
+
+            //}
+            //public static void Mul()
+            //{
+            //    Console.WriteLine("Give me a third number to execute");
+            //    string num3 = Console.ReadLine();
+            //    int.TryParse(num3, out val3);
+
+            //}
+            //public static void Div()
+            //{
+            //    Console.WriteLine("Give me a second number to execute");
+            //    string num2 = Console.ReadLine();
+            //    int.TryParse(num2, out val2);
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+            ////These are peramiters
+            //public void Multiply(int val1, int val2)
+            //    {
+            //        int product = val1 * val2;
+
+            //        Console.WriteLine($"The product of {val1} and {val2} is {product}");
+
+
+            //    }
+            //    public void Addition(int val1, int val2)
+            //    {
+            //        int total = val1 + val2;
+            //        Console.WriteLine($"The product of {val1} added to {val2} is {total}");
+            //    }
+            //    public void Subtraction(int val1, int val2)
+            //    {
+            //        int answer = val1 - val2;
+            //        Console.WriteLine($"The product of {val1} added to {val2} is {answer}");
+
+        
+    
+
     
         
         
