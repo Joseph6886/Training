27ec0533831ce4df913002dc0de5df0abef3dbed
Joseph6886 <dragon6886@msn.com>
2017-07-24 10:49:52 -0400

    Method overload

---
+++ MethodOverloadingPractice1/MethodOverloadingPractice1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingPractice1
+{
+    class Class1
+    {
+    }
+}
