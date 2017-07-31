f2639c0e8cc12ff6ec8b99ec2db7556420fb98a0
Joseph6886 <dragon6886@msn.com>
2017-07-31 16:41:13 -0400

    first part of the day

---
+++ inheritencePractice1/inheritencePractice1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritencePractice1
+{
+
+    public class Employee
+    {
+        //Base Class
+        public string FirstName;
+        public string LastName;
+        public int EmployeeId;
+        public string Email;
+
+        public void PrintFullName()
+        {
+            Console.WriteLine($"My first name is {FirstName} and my last name is {LastName}");
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee ft = new FullTimeEmployee();
+            ft.
+
+
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+
+        public double Salary;
+        public double FullTimeId;
+    }
+
+
+}
