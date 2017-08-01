602c4f79e24457888b6b55daca0d8abbc857c369
Joseph6886 <dragon6886@msn.com>
2017-08-01 11:31:27 -0400

    abstract

---
+++ AbstractPracticeApp1/AbstractPracticeApp1/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPracticeApp1
+{
+    class Car
+    {
+
+        public int milage = 60;
+
+        public string color = "Red";
+
+        private string formula = "a + b";
+
+
+        public int GetMilage()
+        {
+
+            return milage;
+
+        }
+
+        public string GetColor()
+        {
+
+            return color;
+
+        }
+        private string GetEngineFormula()
+        {
+
+            return formula;
+
+        }
+        public string DisplayEngineFormula()
+        {
+
+            return GetEngineFormula();
+        }
+
+
+    }
+}
---
+++ AbstractPracticeApp1/AbstractPracticeApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPracticeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car drive = new Car();
+
+            Console.WriteLine($"Car mileage is {drive.GetMilage()}  ");
+
+            Console.WriteLine($"Car color is {drive.GetColor()}");
+
+            Console.WriteLine($"The secret formula is {drive.DisplayEngineFormula()} ");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
