4ff25604a6fc003e4e2786b76adb708757663355
Joseph6886 <dragon6886@msn.com>
2017-08-02 12:16:42 -0400

    interface2

---
+++ InterfacePractice2/InterfacePractice2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice2
+{
+    class Program
+    {
+        public interface IThink
+        {
+            void Football();
+
+            void Baseball();
+
+
+        }
+
+        public interface IDo
+        {
+
+            void Basketball();
+        }
+
+        public class Sports : IThink, IDo
+        {
+
+
+
+            public void Football()
+            {
+
+                Console.WriteLine("The Browns will be 8-8 this year");
+            }
+
+            public void Baseball()
+            {
+
+                Console.WriteLine("The Indians lost last night");
+            }
+
+            public void Basketball()
+            {
+                Console.WriteLine("The Cavs are going to trade Kyrie");
+
+            }
+
+            
+                
+        }
+            
+
+
+
+        static void Main(string[] args)
+        {
+        Sports s = new Sports();
+        s.Football();
+        s.Baseball();
+        s.Basketball();
+
+        Console.ReadLine();
+
+
+
+                   
+        }
+    }
+}
